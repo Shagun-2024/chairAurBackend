c66926ccaadd055e0d5be68e79ebcaf056406ac0
Shagun Sharma <imshagun05@gmail.com>
2026-05-13 13:45:51 +0530

    setup refresh and access token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,21 +1,33 @@
-import {Router} from "express"
-import { registerUser } from "../controllers/user.controller.js"
-import {upload} from '../middlewares/multer.middleware.js'
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
   upload.fields([
     {
-        name:"avatar",
-        maxCount:1
-    },{
-        name: "coverImage",
-        maxCount:1
-    }
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
   ]),
   registerUser
-)
-// router.route("/login").post(login)
+);
 
-export default router
+router.route("/login").post(loginUser);
+
+// secured routes
+router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
+export default router;
